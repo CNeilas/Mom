7afa1425a9c1fcfbc62346cfb2b1328398830964
nsunklodas <farmeris108@gmail.com>
2024-08-26 13:34:16 +0300

    first-commit

---
+++ main.js
@@ -0,0 +1,87 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterButtons = document.querySelectorAll(".filter button");
+    const products = document.querySelectorAll(".box .product");
+  
+    filterButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        // Get the button text
+        const buttonText = button.textContent.trim();
+  
+        if (buttonText === "Visos paslaugos") {
+          // Show all products if "Visos paslaugos" is clicked
+          products.forEach(product => {
+            product.style.display = "flex";
+          });
+        } else {
+          // Otherwise, map button text to the corresponding class
+          let filterClass = "";
+  
+          switch (buttonText) {
+            case "Pasąmonė":
+              filterClass = "pasamone";
+              break;
+            case "Santykis su savimi":
+              filterClass = "savimi";
+              break;
+            case "Numerologija":
+              filterClass = "numerologija";
+              break;
+            case "Santykiai":
+              filterClass = "santykiai";
+              break;
+            case "Antsvoris":
+              filterClass = "weight";
+              break;
+            case "Asmeninės sesijos":
+              filterClass = "personal";
+              break;
+            case "Kursai":
+              filterClass = "kursai";
+              break;
+          }
+  
+          // Loop through each product and show/hide based on the filterClass
+          products.forEach(product => {
+            if (product.classList.contains(filterClass)) {
+              product.style.display = "flex"; // Show matching products
+            } else {
+              product.style.display = "none"; // Hide non-matching products
+            }
+          });
+        }
+  
+        // Optionally, add active class to the clicked button for styling
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+      });
+    });
+  });
+
+  
+  // Initialize EmailJS
+(function () {
+  emailjs.init("DPiWXqQ7PD9pw3Ey8"); // Replace 'YOUR_USER_ID' with your actual EmailJS user ID
+})();
+
+// Add event listener for the contact form submission
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Send the form data using EmailJS
+    emailjs.sendForm("service_mz9jide", "template_dgbxb3r", this).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Your message has been sent successfully!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        alert(
+          "There was an error sending your message. Please try again later."
+        );
+      }
+    );
+  });
+  
+  //dropdown
---
+++ products/practices.js
@@ -0,0 +1,134 @@
+/*<![CDATA[*/
+(function () {
+    var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+    if (window.ShopifyBuy) {
+      if (window.ShopifyBuy.UI) {
+        ShopifyBuyInit();
+      } else {
+        loadScript();
+      }
+    } else {
+      loadScript();
+    }
+    function loadScript() {
+      var script = document.createElement('script');
+      script.async = true;
+      script.src = scriptURL;
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+      script.onload = ShopifyBuyInit;
+    }
+    function ShopifyBuyInit() {
+      var client = ShopifyBuy.buildClient({
+        domain: '8744af-a2.myshopify.com',
+        storefrontAccessToken: '17996c13a2b28c4450fd977492be37ed',
+      });
+      ShopifyBuy.UI.onReady(client).then(function (ui) {
+        ui.createComponent('product', {
+          id: '9706116514141',
+          node: document.getElementById('product-component-1724082883144'),
+          moneyFormat: '%E2%82%AC%7B%7Bamount_with_comma_separator%7D%7D',
+          options: {
+    "product": {
+      "styles": {
+        "product": {
+          "@media (min-width: 601px)": {
+            "max-width": "calc(25% - 20px)",
+            "margin-left": "20px",
+            "margin-bottom": "50px"
+          }
+        },
+        "button": {
+          "font-size": "13px",
+          "padding-top": "14.5px",
+          "padding-bottom": "14.5px",
+          "border-radius": "40px",
+          "padding-left": "100px",
+          "padding-right": "100px"
+        },
+        "quantityInput": {
+          "font-size": "13px",
+          "padding-top": "14.5px",
+          "padding-bottom": "14.5px"
+        }
+      },
+      "buttonDestination": "modal",
+      "contents": {
+        "img": false,
+        "title": false,
+        "price": false,
+        "options": false
+      },
+      "text": {
+        "button": "Skaityti aprašymą"
+      }
+    },
+    "productSet": {
+      "styles": {
+        "products": {
+          "@media (min-width: 601px)": {
+            "margin-left": "-20px"
+          }
+        }
+      }
+    },
+    "modalProduct": {
+      "contents": {
+        "img": false,
+        "imgWithCarousel": true,
+        "button": false,
+        "buttonWithQuantity": true
+      },
+      "styles": {
+        "product": {
+          "@media (min-width: 601px)": {
+            "max-width": "100%",
+            "margin-left": "0px",
+            "margin-bottom": "0px"
+          }
+        },
+        "button": {
+          "font-size": "13px",
+          "padding-top": "14.5px",
+          "padding-bottom": "14.5px",
+          "border-radius": "40px",
+          "padding-left": "100px",
+          "padding-right": "100px"
+        },
+        "quantityInput": {
+          "font-size": "13px",
+          "padding-top": "14.5px",
+          "padding-bottom": "14.5px"
+        }
+      },
+      "text": {
+        "button": "Add to cart"
+      }
+    },
+    "option": {},
+    "cart": {
+      "styles": {
+        "button": {
+          "font-size": "13px",
+          "padding-top": "14.5px",
+          "padding-bottom": "14.5px",
+          "border-radius": "40px"
+        }
+      },
+      "text": {
+        "total": "Subtotal",
+        "button": "Checkout"
+      }
+    },
+    "toggle": {
+      "styles": {
+        "count": {
+          "font-size": "13px"
+        }
+      }
+    }
+  },
+        });
+      });
+    }
+  })();
+  /*]]>*/
